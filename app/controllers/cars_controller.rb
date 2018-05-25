e98574276f95b605771183c27869ff0417fbd36a
Alan Bischoff <alanlbischoff@gmail.com>
2018-05-25 12:18:53 -0600

    add cars to a list

---
+++ app/controllers/cars_controller.rb
@@ -0,0 +1,51 @@
+class CarsController < ApplicationController
+  def index
+    @cars = Car.order (:year )
+  end
+
+  def show
+    @car = Car.find(params[:id])
+  end
+
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new(car_params)
+
+    if @car.save
+      redirect_to car_path(@car.id)
+    else
+      render :new
+    end
+
+  end
+
+  def edit
+    @car = Car.find(params[:id])
+  end
+
+  def update
+    @car = Car.find(params[:id])
+
+    if @car.update(car_params)
+      redirect_to car_path
+    else
+      render :edit
+    end
+
+  end
+
+  def destroy
+    Car.find(params[:id]).destroy
+    redirect_to cars_path
+  end
+
+
+
+  private
+  def car_params
+    params.require(:car).permit(:make, :model, :year, :color, :msrp, :used)
+  end
+end
